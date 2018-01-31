12fbf519061d413d2a0bc2e2ea36cb77dc4e0096
NKClem <nkuhn09@gmail.com>
2018-01-31 10:45:44 -0500

    added validation to inquirer prompts

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -8,7 +8,7 @@ let userTotal;
 const connection = mysql.createConnection({
 	host: 'localhost',
 	user: 'root',
-	password: '',
+	password: 'password1',
 	database: 'bamazon'
 });
 
@@ -43,29 +43,51 @@ function promptUserInput() {
 		{	//add function to validate that user input is a number
 			type: 'input',
 			name: 'item',
-			message: 'Which item would you like to view? Please select by ID number.'
+			message: 'Which item would you like to view? Please select by ID number.',
+			validate: function checkForIntegers(item) {
+						const reg = /^\d+$/;
+						return reg.test(item) || 'Please enter a numerical value for the ID.';
+					}
 		},
-		{	//add function to validate that user input is a number
+
+		{
 			type: 'input',
 			name: 'itemQuantity',
-			message: 'How many would you like to purchase?'
+			message: 'How many would you like to purchase?',
+			validate: function checkForIntegers(itemQuantity) {
+						const reg = /^\d+$/;
+						return reg.test(itemQuantity) || 'Please enter a numerical value for your amount.';
+					}
 		}
 	]).then(function(answers) {
+
 		let sql = 'SELECT ?? FROM ?? WHERE ?? = ?';
 		let values = ['*', 'products', 'item_id', answers.item];
 		sql = mysql.format(sql, values);
 		connection.query(sql, function(err, results) {
 			if (answers.itemQuantity <= results[0].stock_quantity) {
-				console.log('\nGreat choice!\n')
-				userTotal = results[0].price * answers.itemQuantity;
-				currentDept = results[0].department_name;
+				console.log('\nGreat choice!\n');
+				//give user total 
+				userTotal = parseFloat((results[0].price * answers.itemQuantity).toFixed(2));
 				console.log(`Your total is $${userTotal}\n`);
+				//update database
+				let userQuantity = results[0].stock_quantity - answers.itemQuantity;
+				let sql = 'UPDATE ?? SET ? WHERE ?';
+				let values = ['products', userQuantity, results[0].stock_quantity];
+				sql = mysql.format(sql, values);
+				connection.query(sql, function(err, results, fields) {
+					if (err) throw err;
+					showAllProducts();
+				})
+				
+				//currentDept = results[0].department_name;
+				
 				
 			} else {
 				console.log('\nNot enough in stock. \nPlease pick another item or another amount.\n');
 				promptUserInput();
 			}
-		});
+		});		
 	});
 }
 
